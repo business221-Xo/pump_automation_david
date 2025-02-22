fd9f986e72afd0ccc63873760fd3d4fcbd7f90df
business221-Xo <auction221business@gmail.com>
2025-02-22 07:16:50 -0500

    actualplay

--- actual_b_s_P/pump_fun_py/example_buy.py
+++ actual_b_s_P/pump_fun_py/example_buy.py
@@ -1,7 +1,7 @@
 from pump_fun import buy
 
 # Buy Example
-mint_str = "8BttbUxrmniizKGozjKciWQUoRTnwgpHtEJMB1NTpump"
-sol_in = .02
+mint_str = "C2sGX4Lo8hHCVX3VpWcxD3utCRi1U9Y9Fb5tsgFrpump"
+sol_in = .002
 slippage = 5
 buy(mint_str, sol_in, slippage)
--- actual_b_s_P/pump_fun_py/example_sell.py
+++ actual_b_s_P/pump_fun_py/example_sell.py
@@ -1,7 +1,7 @@
 from pump_fun import sell
 
 # Sell Example
-mint_str = "8BttbUxrmniizKGozjKciWQUoRTnwgpHtEJMB1NTpump"
+mint_str = "C2sGX4Lo8hHCVX3VpWcxD3utCRi1U9Y9Fb5tsgFrpump"
 percentage = 100
 slippage = 5
 sell(mint_str, percentage, slippage)
