4cb37d524bd36f6a60733a74fa7fc3806cc0720a
business221-Xo <auction221business@gmail.com>
2025-06-16 05:43:36 +0300

    Update david_jito.py - Portal

--- l-e/david_jito.py
+++ l-e/david_jito.py
@@ -1,3 +1,101 @@
+# Pump Portal
+# import asyncio
+# import websockets
+# import json
+
+# async def subscribe():
+#   uri = "wss://pumpportal.fun/api/data"
+#   async with websockets.connect(uri) as websocket:
+      
+#       # Subscribing to token creation events
+#       payload = {
+#           "method": "subscribeNewToken",
+#       }
+#       await websocket.send(json.dumps(payload))
+
+#       # Subscribing to migration events
+#       payload = {
+#           "method": "subscribeMigration",
+#       }
+#       await websocket.send(json.dumps(payload))
+
+#       # Subscribing to trades made by accounts
+#       payload = {
+#           "method": "subscribeAccountTrade",
+#           "keys": ["AArPXm8JatJiuyEffuC1un2Sc835SULa4uQqDcaGpAjV"]  # array of accounts to watch
+#       }
+#       await websocket.send(json.dumps(payload))
+
+#       # Subscribing to trades on tokens
+#       payload = {
+#           "method": "subscribeTokenTrade",
+#           "keys": ["91WNez8D22NwBssQbkzjy4s2ipFrzpmn5hfvWVe2aY5p"]  # array of token CAs to watch
+#       }
+#       await websocket.send(json.dumps(payload))
+      
+#       async for message in websocket:
+#           print(json.loads(message))
+
+# # Run the subscribe function
+# asyncio.get_event_loop().run_until_complete(subscribe())
+
+# import requests
+ 
+# response = requests.post(url="https://pumpportal.fun/api/trade?api-key=your-api-key-here", data={
+#     "action": "buy",             # "buy" or "sell"
+#     "mint": "your CA here",      # contract address of the token you want to trade
+#     "amount": 100000,            # amount of SOL or tokens to trade
+#     "denominatedInSol": "false", # "true" if amount is amount of SOL, "false" if amount is number of tokens
+#     "slippage": 10,              # percent slippage allowed
+#     "priorityFee": 0.00005,        # amount used to enhance transaction speed
+#     "pool": "auto"               # exchange to trade on. "pump", "raydium", "pump-amm", "launchlab", "raydium-cpmm", "bonk" or "auto"
+# })
+ 
+# data = response.json()           # Tx signature or error(s)
+
+
+# import requests
+# from solders.transaction import VersionedTransaction
+# from solders.keypair import Keypair
+# from solders.commitment_config import CommitmentLevel
+# from solders.rpc.requests import SendVersionedTransaction
+# from solders.rpc.config import RpcSendTransactionConfig
+
+# response = requests.post(url="https://pumpportal.fun/api/trade-local", data={
+#     "publicKey": "Your public key here",
+#     "action": "buy",             # "buy" or "sell"
+#     "mint": "token CA here",     # contract address of the token you want to trade
+#     "amount": 100000,            # amount of SOL or tokens to trade
+#     "denominatedInSol": "false", # "true" if amount is amount of SOL, "false" if amount is number of tokens
+#     "slippage": 10,              # percent slippage allowed
+#     "priorityFee": 0.005,        # amount to use as priority fee
+#     "pool": "auto"               # exchange to trade on. "pump", "raydium", "pump-amm", 'launchlab', 'raydium-cpmm', 'bonk', or "auto"
+# })
+
+# keypair = Keypair.from_base58_string("Your base 58 private key here")
+# tx = VersionedTransaction(VersionedTransaction.from_bytes(response.content).message, [keypair])
+
+# commitment = CommitmentLevel.Confirmed
+# config = RpcSendTransactionConfig(preflight_commitment=commitment)
+# txPayload = SendVersionedTransaction(tx, config)
+
+# response = requests.post(
+#     url="Your RPC Endpoint here - Eg: https://api.mainnet-beta.solana.com/",
+#     headers={"Content-Type": "application/json"},
+#     data=SendVersionedTransaction(tx, config).to_json()
+# )
+# txSignature = response.json()['result']
+# print(f'Transaction: https://solscan.io/tx/{txSignature}')
+
+
+
+
+
+
+
+
+
+
 import time
 import requests
 import websockets
